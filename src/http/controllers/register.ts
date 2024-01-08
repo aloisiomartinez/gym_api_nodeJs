51f13a7031a841af55f3dba78ae49ad513c74232
aloisiomartinez <aloisiojacobs36@gmail.com>
2024-01-08 14:51:06 -0300

    creating tests for users

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma"
 import { hash } from 'bcryptjs'
 import { z } from "zod"
 import { RegisterUseCase } from '@/use-cases/register'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 
 export async function register(request: FastifyRequest, reply: FastifyReply)  {
@@ -29,7 +29,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply)  {
       return reply.status(409).send({ message: err.message})
     }
 
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma"
 import { hash } from "bcryptjs"
 import { UserAlreadyExistsError } from "./errors/user-already-exists";
+import { User } from "@prisma/client";
+import { UsersRepository } from "@/repositories/users-repository";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -8,32 +10,36 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(
-    private usersRepository: any,
+    private usersRepository: UsersRepository,
   ) {}
 
   async execute({
     name,
     email,
     password
-  }: RegisterUseCaseRequest){
+  }: RegisterUseCaseRequest) :Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
   
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      }
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
   
     if(userWithSameEmail) {
       throw new UserAlreadyExistsError()
     }
   
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash
     })
+
+    return {
+      user,
+    }
 }
 }
